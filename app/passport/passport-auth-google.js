2898ec4742f7c4cb31ae9a95aed4ebba519d555b
vikas <cheenupal26122002@gmail.com>
2023-10-02 22:52:51 +0530

    This is my Node.js_authentication  project

---
+++ app/passport/passport-auth-google.js
@@ -0,0 +1,48 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+const crypto = require('crypto');
+const User = require('../models/user');
+
+
+
+passport.use(
+    new googleStrategy(
+      {
+        clientID: "540191886362-trq0daenof22p3pe1fpb6v4a88adp531.apps.googleusercontent.com",
+        clientSecret: "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",
+        callbackURL: "http://localhost:8500/auth/google/callback",
+      },
+      async function (accessToken, refreshToken, profile, done) {
+        try {
+          // find a user
+          const user = await User.findOne({ email: profile.emails[0].value }).exec();
+  
+          console.log(accessToken, refreshToken);
+          console.log(profile);
+  
+          if (user) {
+            // if found, set this user as req.user
+            return done(null, user);
+          } else {
+            // if not found, create the user and set it as req.user
+            const newUser = await User.create({
+              username: profile.displayName,
+              email: profile.emails[0].value,
+              password: crypto.randomBytes(20).toString("hex"),
+            });
+  
+            return done(null, newUser);
+          }
+        } catch (err) {
+          console.log("error in google strategy-passport", err);
+          return done(err);
+        }
+      }
+    )
+  );
+  
+         
+
+
+module.exports = passport;
